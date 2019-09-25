4da7cbaf662c60c0ae6e1563d9644827588786fd
Christopher Hartmann <chriha84@gmail.com>
2019-09-25 18:26:54 +0200

    add config handling

---
+++ src/Libraries/Config.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Chriha\ProjectCLI\Libraries;
+
+use Chriha\ProjectCLI\Helpers;
+use Illuminate\Support\Arr;
+use PHLAK\SemVer\Version;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
+
+class Config
+{
+
+    /** @var array */
+    protected $config;
+
+    /** @var string */
+    protected $file = '.project.yml';
+
+
+    public function get( string $path )
+    {
+        if ( empty( $this->config ) )
+        {
+            $this->loadConfig();
+        }
+
+        return Arr::get( $this->config, $path );
+    }
+
+    public function set( string $path, $value ) : self
+    {
+        $this->config = Arr::set( $this->config, $path, $value );
+
+        return $this;
+    }
+
+    private function loadConfig()
+    {
+        if ( ! Helpers::app( 'project.inside' ) ) return;
+
+        $path = Helpers::projectPath( $this->file );
+
+        if ( ! $path || ! file_exists( $path ) )
+        {
+            Helpers::abort( "Unable to find project config '{$this->file}'" );
+        }
+
+        try
+        {
+            $this->config = Yaml::parse( file_get_contents( $path ) );
+        }
+        catch ( ParseException $e )
+        {
+            Helpers::abort( "Unable to parse project config '{$this->file}'" );
+        }
+    }
+
+    public function version( Version $version = null ) : Version
+    {
+        if ( $version )
+        {
+            $this->set( 'version', $version );
+        }
+
+        return ( $version = $this->get( 'version' ) )
+            ? new Version( $version ) : new Version;
+    }
+
+    public function __destruct()
+    {
+        $this->save();
+    }
+
+    private function save()
+    {
+        if ( empty( $this->config ) ) return;
+
+        $config = $this->config;
+
+        $config['version'] = $this->version()->prefix();
+
+        ksort( $config );
+
+        $parsed = Yaml::dump( $config );
+
+        file_put_contents( Helpers::projectPath( $this->file ), $parsed );
+    }
+
+}
