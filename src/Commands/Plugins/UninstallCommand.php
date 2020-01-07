66f780cf98d2f5143ccbf1b6351415f35f8fc473
Christopher Hartmann <chriha84@gmail.com>
2020-01-08 00:52:55 +0100

    add command to uninstall plugin

--- src/Commands/Plugins/UninstallCommand.php
+++ src/Commands/Plugins/UninstallCommand.php
@@ -3,6 +3,8 @@
 namespace Chriha\ProjectCLI\Commands\Plugins;
 
 use Chriha\ProjectCLI\Commands\Command;
+use Chriha\ProjectCLI\Helpers;
+use Symfony\Component\Console\Input\InputArgument;
 
 class UninstallCommand extends Command
 {
@@ -11,12 +13,33 @@ class UninstallCommand extends Command
     protected static $defaultName = 'plugins:uninstall';
 
     /** @var string */
-    protected $description = 'Uninstall specified plugins';
+    protected $description = 'Uninstall specified plugin';
 
+
+    public function configure() : void
+    {
+        $this->addArgument(
+            'name',
+            InputArgument::REQUIRED,
+            'The name of the plugin you would like to uninstall'
+        );
+    }
 
     public function handle() : void
     {
-        //
+        $plugins = Helpers::app('plugins') ?? [];
+        $name    = $this->argument('name');
+        $path    = Helpers::pluginsPath($name);
+
+        if ( ! isset($plugins[$name]) && ! is_dir($path)) {
+            $this->abort(sprintf('Plugin <options=bold>%s</> is not installed', $name));
+        }
+
+        if ( ! Helpers::rmdir($path)) {
+            $this->abort('Unable to uninstall plugin.');
+        }
+
+        $this->info('Plugin successfully uninstalled!');
     }
 
 }
