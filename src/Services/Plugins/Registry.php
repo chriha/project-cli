4bfad07e6ce059da073792696460f8b5b7906caf
Christopher Hartmann <chriha84@gmail.com>
2020-02-19 13:07:38 +0100

    add debugging message for failed registry connection

--- src/Services/Plugins/Registry.php
+++ src/Services/Plugins/Registry.php
@@ -22,7 +22,7 @@ class Registry
         try {
             $result = $client->request('GET', self::$url . '/' . urlencode($name));
         } catch (ConnectException | RequestException $e) {
-            Helpers::abort('Unable to connect to registry. Please try again later.');
+            Helpers::abort('Unable to connect to registry. Please try again later.', $e);
             exit;
         } catch (Exception $e) {
             Helpers::abort('Plugin could not be found');
@@ -32,7 +32,7 @@ class Registry
         $info = json_decode($result->getBody()->getContents(), true)['data'] ?? null;
 
         if ( !$info) {
-            Helpers::abort('Unable to get plugin info. Please try again later.');
+            Helpers::abort('Unable to get plugin info. Please try again later.', 'Invalid response format.');
         }
 
         return new Plugin($info);
@@ -46,7 +46,7 @@ class Registry
         try {
             $result = $client->request('GET', self::$url . $query);
         } catch (ConnectException | RequestException $e) {
-            Helpers::abort('Unable to connect to registry. Please try again later.');
+            Helpers::abort('Unable to connect to registry. Please try again later.', $e);
             exit;
         }
 
