6d8a08dc3fb412d406e04cbe13c30ff554fc3ffb
Christopher Hartmann <chriha84@gmail.com>
2020-03-10 01:29:58 +0100

    add ability to easily install required plugins via existing cmd

--- src/Commands/Plugins/InstallCommand.php
+++ src/Commands/Plugins/InstallCommand.php
@@ -3,9 +3,13 @@
 namespace Chriha\ProjectCLI\Commands\Plugins;
 
 use Chriha\ProjectCLI\Commands\Command;
+use Chriha\ProjectCLI\Exceptions\Plugins\NotFoundException;
 use Chriha\ProjectCLI\Helpers;
+use Chriha\ProjectCLI\Libraries\Config\Project;
 use Chriha\ProjectCLI\Services\Git;
+use Chriha\ProjectCLI\Services\Plugins\Plugin;
 use Chriha\ProjectCLI\Services\Plugins\Registry;
+use Exception;
 use Symfony\Component\Console\Input\InputArgument;
 
 class InstallCommand extends Command
@@ -20,7 +24,7 @@ class InstallCommand extends Command
 
     public function configure() : void
     {
-        $this->addArgument('name', InputArgument::REQUIRED, 'The name of the plugin');
+        $this->addArgument('name', InputArgument::OPTIONAL, 'The name of the plugin');
     }
 
     public function handle(Git $git) : void
@@ -28,26 +32,80 @@ class InstallCommand extends Command
         $plugins = Helpers::app('plugins') ?? [];
         $name    = $this->argument('name');
 
+        if ( ! $this->argument('name')) {
+            $missing = $this->checkForRequiredPlugins($plugins);
+
+            if (empty($missing)) {
+                $this->warn('No missing plugins found.');
+
+                return;
+            }
+
+            foreach ($missing as $name) {
+                try {
+                    $plugin = Registry::get($name);
+                    $this->install($git, $plugin);
+                    $this->info(
+                        sprintf('Plugin <options=bold>%s</> successfully installed!', $name)
+                    );
+                } catch (NotFoundException $exception) {
+                    $this->error(
+                        sprintf('Required plugin <options=bold>%s</> does not exist', $name)
+                    );
+                } catch (Exception $exception) {
+                    $this->logger->error($exception);
+                    $this->error(
+                        sprintf('Required plugin <options=bold>%s</> could not be installed', $name)
+                    );
+                }
+            }
+
+            exit;
+        }
+
         if (isset($plugins[$name])) {
             $this->abort(sprintf('Plugin <options=bold>%s</> already installed', $name));
         }
 
         $plugin = Registry::get($name);
-        $path   = Helpers::pluginsPath($plugin->name);
+        $this->install($git, $plugin);
+        $this->info(sprintf('Plugin <options=bold>%s</> successfully installed!', $name));
+    }
 
-        if (!$git->clone($plugin->source, $path)) {
+    protected function install(Git $git, Plugin $plugin) : void
+    {
+        $path = Helpers::pluginsPath($plugin->name);
+
+        if ( ! $git->clone($plugin->source, $path)) {
             $this->abort('Unable to download plugin.');
         }
 
-        if( !$git->checkout($plugin->version, $path)) {
-            $this->error('Unable to find plugin version. Reverting ...');
+        if ( ! $git->checkout($plugin->version, $path)) {
             Helpers::rmdir($path);
-            exit;
+            $this->abort('Unable to find plugin version. Reverting ...');
         }
 
         Registry::incrementInstallations($plugin);
-        $this->info('Plugin successfully installed!');
-        $plugin->asListItem();
+    }
+
+    protected function checkForRequiredPlugins(array $available) : array
+    {
+        $config  = new Project();
+        $missing = [];
+
+        if ( ! $config->hasConfig() || empty($plugins = $config->get('require'))) {
+            return [];
+        }
+
+        foreach ($plugins as $name) {
+            if (isset($available[$name])) {
+                continue;
+            }
+
+            $missing[] = $name;
+        }
+
+        return $missing;
     }
 
 }
