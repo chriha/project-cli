51397c5ec5405775769015bb5678d335d7b41a27
Christopher Hartmann <chriha84@gmail.com>
2020-05-11 14:04:53 +0200

    correct variables in build script

--- build.sh
+++ build.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
-if [ ! -z "$(git status --porcelain)" ]; then
-    printf "You have uncommitted changes\n"
-    exit
-fi
+#if [ ! -z "$(git status --porcelain)" ]; then
+#    printf "You have uncommitted changes\n"
+#    exit
+#fi
 
 CURRENT=$(git describe --tags `git rev-list --tags --max-count=1`)
 
-read -p "new version: "  version_new
+read -p "new version: "  NEW
 
-if [ -z $version_new ]; then
+if [ -z "$NEW" ]; then
     printf "specify a new version!\n"
     exit
 fi
 
-sed -i '.bak' "s/$version_old/$version_new/" bin/project
-sed -i '.bak' "s/$version_old/$version_new/" composer.json
+sed -i '.bak' "s/$CURRENT/$NEW/" bin/project
+sed -i '.bak' "s/$CURRENT/$NEW/" composer.json
 
 rm -f composer.json.bak bin/project.bak
 
 box compile
+
+./project --version
 
 printf "commit your changes and create a new release!\n"
