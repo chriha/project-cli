caa5da077c1abc2271b4fb07031e1ec724392b87
Christopher Hartmann <chriha84@gmail.com>
2021-01-04 18:00:56 +0100

    fix: don't use backup files when sed'ing

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 VERSION=${1}
 
-sed -i '.bak' "s/CURRENT_VERSION/$VERSION/" bin/project
-sed -i '.bak' "s/CURRENT_VERSION/$VERSION/" composer.json
+sed -i "s/CURRENT_VERSION/$VERSION/" bin/project
+sed -i "s/CURRENT_VERSION/$VERSION/" composer.json
 
 composer install
 box compile
